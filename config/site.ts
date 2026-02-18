88e5bf0c1bf7d44846156a9ff0eba7f668ddb596
AtalaBaris <barisatala@outlook.com>
2026-02-18 09:22:33 +0300

    fix

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,21 @@
-export const siteConfig = {
+export type SiteConfig = {
+  name: string;
+  authorName: string;
+  username: string;
+  description: string;
+  url: string;
+  links: {
+    twitter: string;
+    github: string;
+    templateRepo: string;
+  };
+  ogImage: string;
+  iconIco: string;
+  logoIcon: string;
+  keywords: string[];
+};
+
+export const siteConfig: SiteConfig = {
   name: "Baris Atala | AtalaDev",
   authorName: "Barış Atala",
   username: "barisatala",
@@ -9,6 +26,12 @@ export const siteConfig = {
     github: "https://github.com/AtalaBaris",
     templateRepo: "https://github.com/namanbarkiya/minimal-next-portfolio",
   },
+  ogImage:
+    "https://res.cloudinary.com/dvt5vkfwz/image/upload/v1767384721/naman_portfolio_og_image.png",
+  iconIco:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
+  logoIcon:
+    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
 
   keywords: [
     "Barış Atala",
